9cb0a3c3131d074a1e3c792c1e129437294819db
setiawanjoko <setyawanj65@gmail.com>
2023-08-12 19:29:47 +0700

    Validator and services

--- src/server.js
+++ src/server.js
@@ -1,21 +1,30 @@
-const Hapi = require('@hapi/hapi')
-const routes = require('./routes')
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
-          origin: ['*']
-      }
-    }
-  })
+        origin: ['*'],
+      },
+    },
+  });
 
-  server.route(routes)
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
 
-  await server.start()
-  console.log(`Server berjalan pada ${server.info.uri}`)
-}
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
 
-init()
+init();
